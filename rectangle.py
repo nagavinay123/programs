6e55303519574501606e54ebfe624d33ad02e29b
nagavinay123 <96177013+nagavinay123@users.noreply.github.com>
2022-11-06 09:32:56 +0530

    Add files via upload
    added rec code

---
+++ rectangle.py
@@ -0,0 +1,6 @@
+class rectangle:
+    def area(self,l,b):
+        print("Area of rectangle=", l*b)
+    def perimeter(self,l,b):
+        print("Perimeter of the rectangle=",2*(l+b))
+y=rectangle()
